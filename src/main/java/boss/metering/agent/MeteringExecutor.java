a77d4cb1552ef1fabe799545cda5c2f6f9bcd05f
tusharsaran <tushar_saran@persistent.co.in>
2015-07-23 16:15:38 +0530

    modified maven project

--- src/main/java/boss/metering/agent/MeteringExecutor.java
+++ src/main/java/boss/metering/agent/MeteringExecutor.java
@@ -1,10 +1,52 @@
+/* Copyright (c) 2011, IBM and/or its affiliates. All rights reserved. */
+
+
+/*
+	PRIVATE CLASSES
+	<class name / None if no private class>
+
+	NOTES
+	<Information about what functionality this class have  >
+
+	MODIFIED      (MM/DD/YY)
+	<author name>  mm/dd/yy – information about changes made
+	<author name>  mm/dd/yy - Creation
+
+*/
+
+
+
 package boss.metering.agent;
+
+
+
+/**
+* <Description about what this class do, which class it extends or implements if applicable>
+* 
+ * @author Persistent Systems Ltd. 
+ * 
+ */
 
 public class MeteringExecutor {
 
 	public MeteringExecutor() {
 		// TODO Auto-generated constructor stub
 	System.out.println("Testing Execution");		
+	}
+	
+	
+	
+	/**
+	    * < In detail information what this function does >
+	    * @param <input param name>
+	    *           <information about that parameter> 
+	    * 
+	    * @return
+	    * @throws < Name of the exception it throws>
+	*/
+
+	public void getMeteringUsage(){
+		System.out.println("Inside Metering Usage Method");		
 	}
 
 }
